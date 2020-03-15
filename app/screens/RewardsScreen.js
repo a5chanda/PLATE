27db653eb0191b522e612714af8f16d335677ca6
a5chanda <abhinav.chanda99@gmail.com>
2020-03-14 23:14:13 -0400

    Added galio card

--- app/screens/RewardsScreen.js
+++ app/screens/RewardsScreen.js
@@ -1,57 +1,25 @@
-// import * as React from 'react';
-// import { Button, View, Text } from 'react-native';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import {CameraScreen} from './CameraScreen';
-
 import * as React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
-//import CameraButton from '../components/CameraButton';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import {CameraScreen} from './CameraScreen';
 import CameraButton from '../components/CameraButton';
-
-
+import {RecipeCard} from '../components/shared/RecipeCard';
+import {Card, theme} from "galio-framework";
 
 export default function RewardsScreen() {
-  return (     
-    <CameraButton></CameraButton>
+  return (  
+      
+     <ScrollView>
+    <RecipeCard></RecipeCard>
+    </ScrollView>  
+   
   );
 }
 
 
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fafafa',
-  },
-  contentContainer: {
-    paddingTop: 15,
-  },
-  optionIconContainer: {
-    marginRight: 12,
-  },
-  option: {
-    backgroundColor: '#fdfdfd',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: '#ededed',
-  },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optionText: {
-    fontSize: 15,
-    alignSelf: 'flex-start',
-    marginTop: 1,
-  },
-});
